49c2795b164188afc30f50dac46d7803c67ba792
worasit <worasit.daimongkol@thoughtworks.com>
2022-11-01 17:03:21 +0700

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -57,15 +57,15 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Tab 1 - Worasit</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>Tab 2 - Kaiwanyawit</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel>Tab 3 - Pakorn</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
